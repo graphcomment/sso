4016a9f07d6b804abfafc40b67aa63796fc99bf0
David DJIAN <david@traceweb.fr>
2018-08-28 15:26:31 +0200

    examples in javascript and php

---
+++ javascript/example.js
@@ -0,0 +1,37 @@
+var GC_SECRET = "12345";
+var GC_PUBLIC = "abcde";
+
+function gcSignon(user) {
+
+  var gcData = {
+    id: user.id, // required unique
+    username: user.username, // required unique
+    email: user.email, // required unique
+
+    bio: user.bio, // (optionnal) description
+    picture: user.picture // (optionnal) full url only
+  };
+
+  var gcStr = JSON.stringify(gcData);
+  var timestamp = Math.round(+new Date() / 1000);
+
+  /*
+   * Note that `Buffer` is part of node.js
+   * For pure Javascript or client-side methods of
+   * converting to base64, refer to this link:
+   * http://stackoverflow.com/questions/246801/how-can-you-encode-a-string-to-base64-in-javascript
+   */
+  var message = new Buffer(gcStr).toString('base64');
+
+  /*
+   * CryptoJS is required for hashing (included in dir)
+   * https://code.google.com/p/crypto-js/
+   */
+  var result = CryptoJS.HmacSHA1(message + " " + timestamp, GC_SECRET);
+  var hexsig = CryptoJS.enc.Hex.stringify(result);
+
+  return {
+    pubKey: GC_PUBLIC,
+    auth: message + " " + hexsig + " " + timestamp
+  };
+}
---
+++ php/example.php
@@ -0,0 +1,44 @@
+<?php
+
+define('GC_SECRET', '123456');
+define('GC_PUBLIC', 'abcdef');
+
+$data = array(
+        "id" => $user["id"], // required unique
+        "username" => $user["username"],// required unique
+        "email" => $user["email"],// required unique
+        "bio" => $user["bio"],// (optionnal) description
+        "picture" => $user["picture"]// (optionnal) full url only
+    );
+
+function gc_hmacsha1($data, $key) {
+    $blocksize=64;
+    $hashfunc='sha1';
+    if (strlen($key)>$blocksize)
+        $key=pack('H*', $hashfunc($key));
+    $key=str_pad($key,$blocksize,chr(0x00));
+    $ipad=str_repeat(chr(0x36),$blocksize);
+    $opad=str_repeat(chr(0x5c),$blocksize);
+    $hmac = pack(
+                'H*',$hashfunc(
+                    ($key^$opad).pack(
+                        'H*',$hashfunc(
+                            ($key^$ipad).$data
+                        )
+                    )
+                )
+            );
+    return bin2hex($hmac);
+}
+
+$message = base64_encode(json_encode($data));
+$timestamp = time();
+$hmac = gc_hmacsha1($message . ' ' . $timestamp, GC_SECRET);
+?>
+
+<script type="text/javascript">
+var gc_config = function() {
+    this.page.remote_auth_s3 = "<?php echo "$message $hmac $timestamp"; ?>";
+    this.page.api_key = "<?php echo GC_PUBLIC; ?>";
+}
+</script>
